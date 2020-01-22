43743a9a0d61ac2da6d9938c433a57a2afad6ea8
Jose Mamani <JOSEMLI@example.com>
2020-01-22 09:38:23 -0500

    mostrar puntos de calor y tambien eliminar puntos vistos... cambiar en modo calor y no mapa de calor
    Signed-off-by: Jose Mamani <JOSEMLI@example.com>

--- app/src/main/java/com/example/android_u1_tema5/MapaCalor.java
+++ app/src/main/java/com/example/android_u1_tema5/MapaCalor.java
@@ -3,19 +3,28 @@ package com.example.android_u1_tema5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MapaCalor extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+  TileOverlay mOverlay;
+
+
   GoogleMap mapa; List<Marker> markerList = new ArrayList<>(); int id = 0;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,16 @@ public class MapaCalor extends AppCompatActivity implements OnMapReadyCallback, 
         return true;
       }
     });
+//desde un punto se desaparecee las marcas
+    mapa.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+      @Override
+      public void onCameraIdle() {
+        CameraPosition position = mapa.getCameraPosition();
+        float zoom = position.zoom;
+        for(Marker marker:markerList)
+          marker.setVisible(zoom>=12);
+      }
+    });
   }
   @Override
   public void onMapClick(LatLng latLng) {
@@ -51,5 +70,24 @@ public class MapaCalor extends AppCompatActivity implements OnMapReadyCallback, 
     );
     markerList.add(Mymarker);
     id++;
+  }
+  //mapas de calor
+  public void mapacalor(View view) {
+    if(!markerList.isEmpty()) {
+      HeatmapTileProvider mProvider;
+      ArrayList<LatLng> lista = new ArrayList<>();
+      for (Marker marker : markerList)
+        lista.add(marker.getPosition());
+      mProvider = new HeatmapTileProvider.Builder()
+          .data(lista)
+          .build();
+      mOverlay = mapa.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+  }
+//eliminar puntos de mapas de calor
+  public void limpiar(View view) {
+
+    if(mOverlay != null)
+      mOverlay.remove();
   }
 }
